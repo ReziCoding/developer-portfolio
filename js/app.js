475e5ea47c51df13c6d30b621790c54fb239ec92
Gavin Meche <gavinmeche23@gmail.com>
2026-04-28 14:49:06 -0500

    Add JS scroll reveal animations

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const revealElements = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+    revealElements.forEach((element) => {
+        const windowHeight = window.innerHeight;
+        const revealTop = element.getBoundingClientRect().top;
+        const revealPoint = 120;
+
+        if (revealTop < windowHeight - revealPoint) {
+            element.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
