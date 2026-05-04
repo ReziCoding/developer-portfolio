0286b7df06eddc844617d5ec0432c162cb803fb4
Gavin Meche <gavinmeche23@gmail.com>
2026-05-04 08:44:13 -0500

    Add animated scanline neon particles and cursor effects

--- js/app.js
+++ js/app.js
@@ -14,3 +14,16 @@ function revealOnScroll() {
 
 window.addEventListener("scroll", revealOnScroll);
 window.addEventListener("load", revealOnScroll);
+
+const particleContainer = document.querySelector(".particle-container");
+
+for (let i = 0; i < 40; i++) {
+    const particle = document.createElement("div");
+    particle.classList.add("particle");
+
+    particle.style.left = Math.random() * 100 + "%";
+    particle.style.animationDuration = (Math.random() * 8 + 5) + "s";
+    particle.style.animationDelay = Math.random() * 5 + "s";
+
+    particleContainer.appendChild(particle);
+}
